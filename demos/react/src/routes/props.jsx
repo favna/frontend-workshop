784870e7d4de583f6acd43fe05c461111b64338d
Jeroen Claassens <jeroen.claassens@rws.nl>
2024-09-24 15:47:55 +0200

    update props example

--- demos/react/src/routes/props.jsx
+++ demos/react/src/routes/props.jsx
@@ -1,19 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
+import './props.css';
+
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/props')({
-  component: Props,
-})
+	component: Props
+});
 
 function Props() {
-  return (
-    <>
-      <Button onClick={() => console.log('Clicked!')} label="Click me!" />
-    </>
-  )
+	return (
+		<>
+			<GitHubButton onClick={() => console.log('Clicked!')} label="I'm a GitHub styled button!" />
+			<StripeButton onClick={() => console.log('Clicked!')} label="I'm a Stripe styled button!" />
+		</>
+	);
 }
 
-function Button(props) {
-  return (
-    <button onClick={props.handleClick}>{props.label}</button>
-  )
+function GitHubButton(props) {
+	return (
+		<button className="github-button" onClick={props.handleClick}>
+			{props.label}
+		</button>
+	);
+}
+
+function StripeButton(props) {
+	return (
+		<button className="stripe-button" onClick={props.handleClick}>
+			{props.label}
+		</button>
+	);
 }
